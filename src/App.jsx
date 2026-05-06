023ff99c4d873da9969de01d0934121a92060f3b
ACastilleja <castillejaarturo@gmail.com>
2026-05-06 18:02:37 -0400

    Part 2 added completed checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,22 @@ function App() {
     setTodoList(previous=>[newTodo,...previous])
 
   }
-  function completeTodo () {
+  const completeTodo = (id)=>{
+    const updatedTodoList = todoList.map((todo)=>{
+      if (todo.id === id){
+        return {...todo, isCompleted: true};
+      }
+      return todo;
+    });
+    setTodoList(updatedTodoList);
 
+  };
 
-
-  }
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={addTodo}/>
-      <TodoList todoList={todoList}/>
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo}/>
       
     </div>
   )

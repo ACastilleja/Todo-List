58920d3338513c8aed49cfa92a526a7a8595bdb4
ACastilleja <castillejaarturo@gmail.com>
2026-05-19 23:22:00 -0400

    added header component and upadted app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
-
+import Header from './shared/Header';
+import TodosPage from './features/Todos/TodosPage';
 
   
 
 function App() {
   
-
-  return ;
+  return(
+    <div>
+      <Header/>
+      <TodosPage/>
+    </div>
+  );
 };
 
 export default App;

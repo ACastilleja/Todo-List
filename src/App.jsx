eff6e411866d383516a41bac7528976562397d8f
ACastilleja <castillejaarturo@gmail.com>
2026-05-19 23:11:44 -0400

    Moved Todos to TodosPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,12 @@
-
-import { useState } from 'react'
 import './App.css'
-import TodoList from './features/TodoList/TodoList';
-import TodoForm from './features/TodoForm';
-
 
 
   
 
 function App() {
-  const [todoList, setTodoList]=useState([]);
+  
 
-  const addTodo = (todoTitle)=>{
+  return ;
+};
 
-    const newTodo = {id:Date.now(), title: todoTitle, isCompleted: false };
-    setTodoList(previous=>[newTodo,...previous])
-
-  }
-  const completeTodo = (id)=>{
-    const updatedTodoList = todoList.map((todo)=>{
-      if (todo.id === id){
-        return {...todo, isCompleted: true};
-      }
-      return todo;
-    });
-    setTodoList(updatedTodoList);
-
-  };
-  const updateTodo =(editedTodo)=>{
-    const updatedTodos = todoList.map((todo)=>{
-      return todo.id === editedTodo.id ? editedTodo : todo;
-    });
-    setTodoList(updatedTodos);
-  }
-
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <TodoForm onAddTodo={addTodo}/>
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo}/>
-      
-    </div>
-  )
-}
-
-export default App
+export default App;
---
+++ src/features/Todos/TodosPage.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import TodoList from './TodoList/TodoList';
+import TodoForm from './TodoForm';
+
+function TodosPage() {
+  const [todoList, setTodoList]=useState([]);
+
+  const addTodo = (todoTitle)=>{
+
+    const newTodo = {id:Date.now(), title: todoTitle, isCompleted: false };
+    setTodoList(previous=>[newTodo,...previous])
+
+  }
+  const completeTodo = (id)=>{
+    const updatedTodoList = todoList.map((todo)=>{
+      if (todo.id === id){
+        return {...todo, isCompleted: true};
+      }
+      return todo;
+    });
+    setTodoList(updatedTodoList);
+
+  };
+  const updateTodo =(editedTodo)=>{
+    const updatedTodos = todoList.map((todo)=>{
+      return todo.id === editedTodo.id ? editedTodo : todo;
+    });
+    setTodoList(updatedTodos);
+  }
+
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <TodoForm onAddTodo={addTodo}/>
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} onUpdateTodo={updateTodo}/>
+      
+    </div>
+  )
+}
+
+export default TodosPage;

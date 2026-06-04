84a269c438445fcc8c1933e8a6415303c6d2075c
ACastilleja <castillejaarturo@gmail.com>
2026-06-04 15:06:05 -0400

    Updated header.jsx

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,6 +1,20 @@
+import { useAuth } from "../contexts/AuthContext";
+import Logoff from '../features/Logoff';
+
 function Header () {
+
+    const {isAuthenticated, email}=useAuth();
     return (
+        <header style={{padding:'10px',borderBottom:'1px solid #ccc'}}>
             <h1>Todo List</h1>
+            {isAuthenticated && (
+                <div className="user-profile">
+                    <span>Welcome, {email}!</span>
+                    <Logoff/>
+                </div>
+            )}
+        </header>
+            
     );
 };
 export default Header;

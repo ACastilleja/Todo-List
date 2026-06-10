5e579583a15bec97c2502d40d3633255f96a6bd5
ACastilleja <castillejaarturo@gmail.com>
2026-06-09 20:05:18 -0400

    Added BrowserRouter to Main and fixed App also created HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,18 @@ import './App.css'
 import Header from './shared/Header';
 import TodosPage from './features/Todos/TodosPage';
 import Logon from './features/Logon';
-import { useAuth } from './contexts/AuthContext';
+import { Routes,Route } from 'react-router';
   
 
 function App() {
-  const {isAuthenticated}=useAuth();
-
+  
   return(
     <div>
       <Header />
-      {isAuthenticated ? (
-      <TodosPage/>
-      ) : (
-        <Logon/>
-      )
-    }
+      <Routes>
+        
+      </Routes>
+      
     </div>
   );
 };
